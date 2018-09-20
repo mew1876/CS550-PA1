9c3d0f92e4a9b21a5cd3eeb21278d2526ca7c5f8
mew1876 <mew1876@gmail.com>
2018-09-20 11:05:42 -0500

    Created Peer class

---
+++ P2P FileShare/Peer.cpp
@@ -0,0 +1,10 @@
+#include "Peer.h"
+
+int Peer::currentPeerID = 0;
+
+Peer::Peer() {
+}
+
+
+Peer::~Peer() {
+}
---
+++ P2P FileShare/Peer.h
@@ -0,0 +1,9 @@
+#pragma once
+class Peer {
+	static int currentPeerID;
+
+public:
+	Peer();
+	~Peer();
+};
+
