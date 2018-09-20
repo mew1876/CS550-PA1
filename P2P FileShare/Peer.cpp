0f085826768019176541099ddd596e39e5e29740
mew1876 <mew1876@gmail.com>
2018-09-20 16:04:37 -0500

    Peer retrieval, added libraries to repo

--- P2P FileShare/Peer.cpp
+++ P2P FileShare/Peer.cpp
@@ -1,10 +1,44 @@
 #include "Peer.h"
+#include <vector>
+#include <iostream>
 
-int Peer::currentPeerID = 0;
+int Peer::currentPeerID = 1;
 
-Peer::Peer() {
+Peer::Peer()
+	: indexClient("localhost", 8000), server(8000 + currentPeerID) {
+	peerID = currentPeerID++;	
+	server.bind("getFile", &getFile);
 }
 
+void Peer::retrieve(std::string filename) {
+	std::vector<int> sources = indexClient.call("search", filename).as<std::vector<int>>();
+	if (sources.size() == 0) {
+		std::cout << "No peers found with " << filename << std::endl;
+	}
+	else {
+		std::cout << "Select a peer ID:" << std::endl;
+		for (auto sourceID : sources) {
+			std::cout << sourceID << " ";
+		}
+		std::cout << std::endl;
+		int sourceID;
+		std::cin >> sourceID;
+	
+		rpc::client peerClient("localhost", 8000 + sourceID);
+		std::ifstream source = peerClient.call("getFile", filename).as<std::ifstream>();
+		std::ofstream destination(getPath() + filename, std::ios::binary);
+		destination << source.rdbuf();
+	}
+}
+
+std::ifstream Peer::getFile(std::string filename) {
+	std::ifstream stream(getPath() + filename, std::ios::binary);
+	return stream;
+}
+
+std::string Peer::getPath() {
+	return "Peers/Peer " + std::to_string(peerID) + "/";
+}
 
 Peer::~Peer() {
 }
--- P2P FileShare/Peer.h
+++ P2P FileShare/Peer.h
@@ -1,9 +1,18 @@
 #pragma once
+#include <fstream>
+#include "rpc/client.h"
+#include "rpc/server.h"
+
 class Peer {
 	static int currentPeerID;
-
+	int peerID;
+	rpc::client indexClient;
+	rpc::server server;
+	std::string getPath();
 public:
 	Peer();
 	~Peer();
+	void retrieve(std::string filename);
+	std::ifstream getFile(std::string filename);
 };
 
