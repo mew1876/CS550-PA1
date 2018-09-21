5be9c0dde5af2f5e3e8625851f53114b41e0351f
jujubochnak <jbochnak@hawk.iit.edu>
2018-09-20 21:54:40 -0500

    Index Server functionality added, added both users'  absolute paths for rpc lib, and removed library from repo

--- P2P FileShare/IndexServer.cpp
+++ P2P FileShare/IndexServer.cpp
@@ -1,9 +1,59 @@
 #include "IndexServer.h"
 
 
-
 IndexServer::IndexServer()
 {
+	//std::unordered_map<std::string, std::vector<int>> fileNameToPeerIDIndex
+	fileNameToPeerIDIndex = {};
+}
+
+std::vector<int> IndexServer::search(std::string fileName) {
+	auto index = fileNameToPeerIDIndex.find(fileName);
+	if (index != fileNameToPeerIDIndex.end()) {
+		return index->second; // vector of PeerID variables
+	}
+	else
+	{
+		return {};
+	}
+}
+void IndexServer::remove(int peerID, std::string fileName) {
+	auto mapIndex = fileNameToPeerIDIndex.find(fileName);
+	if (mapIndex != fileNameToPeerIDIndex.end()) {
+		std::vector<int> peerIDVector = mapIndex->second;
+		for (std::vector<int>::iterator peerIDIndex = peerIDVector.begin(); peerIDIndex != peerIDVector.end();) {
+			if (*peerIDIndex == peerID) {
+				fileNameToPeerIDIndex.erase(mapIndex);
+			}
+		}
+		if (peerIDVector.empty()) {
+			fileNameToPeerIDIndex.erase(mapIndex);
+		}
+	}
+}
+
+void IndexServer::add(int peerID, std::string fileName) {
+	auto mapIndex = fileNameToPeerIDIndex.find(fileName);
+	if (mapIndex != fileNameToPeerIDIndex.end()) {
+		fileNameToPeerIDIndex.insert({ fileName, {peerID} });
+	}
+	else
+	{
+		auto peerIDVector = mapIndex->second;
+		for (auto peerIDIndex = peerIDVector.begin(); peerIDIndex != peerIDVector.end();) {
+			if (*peerIDIndex == peerID) {
+				fileNameToPeerIDIndex.erase(mapIndex);
+			}
+		}
+		if (peerIDVector.empty()) {
+			fileNameToPeerIDIndex.erase(mapIndex);
+		}
+	}
+}
+void IndexServer::add(int peerID, std::string fileNames[]) {
+	for (std::string name : fileNames->begin) {
+		add(peerID, name);
+	}
 }
 
 
--- P2P FileShare/IndexServer.h
+++ P2P FileShare/IndexServer.h
@@ -1,6 +1,14 @@
 #pragma once
+#include <unordered_map>
+#include <vector>
+
 class IndexServer
 {
+	std::unordered_map<std::string, std::vector<int>> fileNameToPeerIDIndex;
+	std::vector<int> search(std::string fileName);
+	void remove(int peerID, std::string fileName);
+	void add(int peerID, std::string fileName);
+	void add(int peerID, std::string fileNames[]);
 public:
 	IndexServer();
 	~IndexServer();
