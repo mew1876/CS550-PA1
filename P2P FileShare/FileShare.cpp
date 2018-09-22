b99fb3d1f12c430e44411e4271a7bfceeda1ba1d
jujubochnak <jbochnak@hawk.iit.edu>
2018-09-22 15:08:23 -0500

    added easy file creation

--- P2P FileShare/FileShare.cpp
+++ P2P FileShare/FileShare.cpp
@@ -3,6 +3,10 @@
 
 #include <iostream>
 
+int currentFileID = 1;
+
+void createFile(int peerID, int fileSizeInKB);
+
 int main(int argc, char* argv[]) {
 	IndexServer server;
 	Peer peer1;
@@ -15,4 +19,18 @@ int main(int argc, char* argv[]) {
 	peer1.add("test.txt");
 	
 	peer2.retrieve("test.txt");
+
+	createFile(1, 2);
+}
+
+void createFile(int peerID, int fileSizeInKB) {
+	std::ofstream file("Peers/Peer " + std::to_string(peerID) + "/File " + std::to_string(currentFileID++) + ".txt");
+	std::srand(std::time(nullptr));
+	for (int KB = 0; KB < fileSizeInKB; KB++) {
+		// write 1KB of random readable character to the file (ASCII: 32 to 126)
+		for (int i = 0; i < 1024; i++) { 
+			file << char((std::rand() % 95) + 32);
+		}
+	}
+	file.close();
 }
